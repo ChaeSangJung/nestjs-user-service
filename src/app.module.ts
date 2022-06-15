5bc8ceffebfa4f48ef718084fe272464d5ac965e
che <shtingstar@hanmail.net>
2022-06-15 15:06:00 +0900

    3장 애플리케이션의 관문 - 인터페이스

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [UsersController],
   providers: [AppService],
 })
 export class AppModule {}
